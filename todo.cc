8fca1614ff9a734e9e4df5382cc94b2b563473aa
tomocy <tomocy.dev@gmail.com>
2020-05-18 13:49:44 +0900

    update User
    User has its password.

--- todo.cc
+++ todo.cc
@@ -3,10 +3,13 @@
 #include <string>
 
 namespace todo {
-User::User(const std::string& id, const std::string& email)
-    : id(id), email(email) {}
+User::User(const std::string& id, const std::string& email,
+           const std::string& password)
+    : id(id), email(email), password(password) {}
 
 const std::string& User::ID() const { return id; }
 
 const std::string& User::Email() const { return email; }
+
+const std::string& User::Password() const { return password; }
 }  // namespace todo
--- todo.h
+++ todo.h
@@ -8,15 +8,19 @@ class User {
  public:
   User() = default;
 
-  User(const std::string& id, const std::string& email);
+  User(const std::string& id, const std::string& email,
+       const std::string& password);
 
   const std::string& ID() const;
 
   const std::string& Email() const;
 
+  const std::string& Password() const;
+
  private:
   std::string id;
   std::string email;
+  std::string password;
 };
 }  // namespace todo
 
