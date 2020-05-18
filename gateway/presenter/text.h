cac2c1c363d82469d0c9f6b25894d07813a01853
tomocy <tomocy.dev@gmail.com>
2020-05-18 15:47:17 +0900

    update presenter::text::Render
    Render implements controller::Render.

--- gateway/presenter/text.h
+++ gateway/presenter/text.h
@@ -1,12 +1,13 @@
 #ifndef TOMOCY_CPP_TODO_GATEWAY_PRESENTER_TEXT_H
 #define TOMOCY_CPP_TODO_GATEWAY_PRESENTER_TEXT_H
 
+#include "gateway/controller/controller.h"
 #include "todo.h"
 
 namespace presenter::text {
-class Renderer {
+class Renderer : public controller::Renderer {
  public:
-  void ShowUser(const todo::User&) const;
+  void ShowUser(const todo::User&) const override;
 };
 }  // namespace presenter::text
 
