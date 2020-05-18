1694abcff34f94bb47f0e95c153dfcd02c452a54
tomocy <tomocy.dev@gmail.com>
2020-05-18 22:29:12 +0900

    update todo entrypoint
    This entrypoint injects todo::UserRepo into controller::cli::App.

--- main/todo/main.cc
+++ main/todo/main.cc
@@ -2,10 +2,12 @@
 
 #include "gateway/controller/cli.h"
 #include "gateway/presenter/text.h"
+#include "infra/memory.h"
 
 int main(int n, const char* const* const args) {
+  auto userRepo = infra::memory::UserRepo();
   const auto ren = presenter::text::Renderer();
-  auto app = controller::cli::App(ren);
+  auto app = controller::cli::App(userRepo, ren);
   app.Run(n, args);
 
   return EXIT_SUCCESS;
