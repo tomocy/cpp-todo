50190b3d33a8de229648a17dc666c5cadfd38a43
tomocy <tomocy.dev@gmail.com>
2020-05-18 14:37:37 +0900

    add controller::cli::App

--- gateway/controller/cli.cc
+++ gateway/controller/cli.cc
@@ -1,1 +1,22 @@
 #include "gateway/controller/cli.h"
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace controller::cli {
+App::App() {}
+
+void App::Run(int n, const char* const* const args) const {
+  auto converted = std::vector<std::string>(args, args + n);
+  Parse(converted);
+}
+
+void App::Parse(const std::vector<std::string>& args) const { ShowHelp(); }
+
+void App::ShowHelp() const {
+  std::cout << "todo" << std::endl;
+  std::cout << "Usage:  [command] args..." << std::endl;
+  std::cout << "Commands:" << std::endl;
+}
+}  // namespace controller::cli
--- gateway/controller/cli.h
+++ gateway/controller/cli.h
@@ -1,6 +1,22 @@
 #ifndef TOMOCY_CPP_TODO_GATEWAY_CONTROLLER_CLI_H
 #define TOMOCY_CPP_TODO_GATEWAY_CONTROLLER_CLI_H
 
-namespace controller::cli {}
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace controller::cli {
+class App {
+ public:
+  App();
+
+  void Run(int n, const char* const* args) const;
+
+ private:
+  void Parse(const std::vector<std::string>&) const;
+
+  void ShowHelp() const;
+};
+}  // namespace controller::cli
 
 #endif
