6363ff09626e6210d2b0e9685c51db8f7724a5bc
tomocy <tomocy.dev@gmail.com>
2020-05-18 13:55:26 +0900

    add usecase bazel package

---
+++ usecase/usecase.cc
@@ -0,0 +1,1 @@
+#include "usecase/usecase.h"
---
+++ usecase/usecase.h
@@ -0,0 +1,6 @@
+#ifndef TOMOCY_CPP_TODO_USECASE_USECASE_H
+#define TOMOCY_CPP_TODO_USECASE_USECASE_H
+
+namespace usecase {}  // namespace usecase
+
+#endif
