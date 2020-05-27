73670f2edd165e15d93c4382475e4deccfe87019
tomocy <tomocy.dev@gmail.com>
2020-05-27 20:31:52 +0900

    add file::User, Store

--- infra/file.cc
+++ infra/file.cc
@@ -1,1 +1,17 @@
 #include "infra/file.h"
+
+#include <string>
+#include <vector>
+
+#include "todo.h"
+
+namespace infra::file {
+User::User(const todo::User& user)
+    : id(user.ID()),
+      email(user.Email()),
+      password(user.Password().ToString()) {}
+}  // namespace infra::file
+
+namespace infra::file {
+Store::Store(const std::vector<User>& users) : users(users) {}
+}  // namespace infra::file
--- infra/file.h
+++ infra/file.h
@@ -1,4 +1,35 @@
 #ifndef TOMOCY_CPP_TODO_INFRA_FILE_H
 #define TOMOCY_CPP_TODO_INFRA_FILE_H
 
+#include <string>
+#include <vector>
+
+#include "todo.h"
+
+namespace infra::file {
+struct User {
+ public:
+  User() = default;
+
+  User(const todo::User& user) noexcept;
+
+  todo::User ToUser() const noexcept;
+
+  std::string id;
+  std::string email;
+  std::string password;
+};
+}  // namespace infra::file
+
+namespace infra::file {
+struct Store {
+ public:
+  Store() = default;
+
+  Store(const std::vector<User>& users);
+
+  std::vector<User> users;
+};
+}  // namespace infra::file
+
 #endif
