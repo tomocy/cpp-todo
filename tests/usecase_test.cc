293d82d64a263e89c1df3454565952dae9c6d586
tomocy <tomocy.dev@gmail.com>
2020-05-27 12:07:56 +0900

    add test for usecase::CreateUser

--- tests/usecase_test.cc
+++ tests/usecase_test.cc
@@ -1,1 +1,20 @@
 #include "usecase/usecase.h"
+
+#include <string>
+#include <tuple>
+
+#include "gtest/gtest.h"
+#include "infra/memory.h"
+
+TEST(CreateUser, Success) {
+  auto repo = infra::memory::UserRepo();
+
+  auto [email, password] =
+      std::tuple{std::string("aiueo"), std::string("aiueo")};
+
+  auto user = usecase::CreateUser(repo).Do(email, password);
+
+  EXPECT_EQ(repo.Users().size(), 1);
+  EXPECT_EQ(user.Email(), email);
+  EXPECT_TRUE(user.Password().Compare(password));
+}
