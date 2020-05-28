23209530d1e3356a128f7b2e00761b6b3dc6d5e0
tomocy <tomocy.dev@gmail.com>
2020-05-28 11:34:38 +0900

    update todo entrypoint
    file::Session is injected into cli::App.

--- main/todo/main.cc
+++ main/todo/main.cc
@@ -10,17 +10,16 @@
 #include "infra/memory.h"
 
 int main(int n, const char* const* const args) {
-  auto authenticatedUserID = std::string("test_authenticated_user_1");
+  auto workspace = std::string(".");
 
-  auto session = infra::memory::Session();
-  session.SetAuthenticatedUserID(authenticatedUserID);
+  auto session = infra::file::Session(workspace);
 
-  auto userRepo = infra::file::UserRepo(".");
+  auto userRepo = infra::file::UserRepo(workspace);
   const auto userRen = presenter::text::UserRenderer();
 
   auto taskRepo = infra::memory::TaskRepo();
   for (auto i = 0; i < 10; ++i) {
-    taskRepo.Save(todo::Task(std::to_string(i), authenticatedUserID,
+    taskRepo.Save(todo::Task(std::to_string(i), "aiueo",
                              "test task " + std::to_string(i)));
   }
   const auto taskRen = presenter::text::TaskRenderer();
